da0337861aeef28ccc28f9437881de664030b556
Jay <joeljoshy224@gmail.com>
2025-10-24 13:00:24 +0000

    Module not found: Can't resolve './BookingDialog'
    i deleted it ,keep it

--- .next/static/chunks/src_app_booking_page_tsx_98502522._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_edb05a9f._.js",
-  "static/chunks/node_modules_0546c79d._.js"
-],
-    source: "dynamic"
-});
