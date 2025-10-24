bf95390acc424f7030f69d21e5f7c37be2e4ca77
Jay <joeljoshy224@gmail.com>
2025-10-24 12:21:24 +0000

    i mean it still shows as popup

---
+++ home/studio-user/src/components/BookingDialog.tsx
@@ -0,0 +1,5 @@
+// This component is intentionally left blank and is no longer in use.
+// The booking flow has been moved to a dedicated page at /book-appointment.
+export default function BookingDialog() {
+  return null;
+}
