b907fea3895eeff5c669c47060cca83f557f8fb4
Jay <joeljoshy224@gmail.com>
2025-10-24 14:58:35 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'cm

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -7,7 +7,7 @@
   "static/chunks/node_modules_date-fns_d7bfa457._.js",
   "static/chunks/node_modules_react-day-picker_dist_index_esm_9fc30424.js",
   "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
-  "static/chunks/node_modules_a0956cd1._.js"
+  "static/chunks/node_modules_d5fd410f._.js"
 ],
     source: "dynamic"
 });
