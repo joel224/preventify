c3b12f8c1065b9d838945016bc15bfb8e2d78099
Jay <joeljoshy224@gmail.com>
2025-10-25 13:28:00 +0000

    Try fixing this error: `Console Error: <a> cannot contain a nested <a>.

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -8,9 +8,9 @@ import {
   Dialog,
   DialogContent,
   DialogFooter,
+  DialogHeader,
   DialogTitle,
   DialogTrigger,
-  DialogClose,
 } from "@/components/ui/dialog";
 import Image from "next/image";
 import ShimmerText from "./ShimmerText";
@@ -72,7 +72,9 @@ const SubscriptionButton = () => {
         </div>
       </DialogTrigger>
       <DialogContent className="w-[95vw] max-w-lg bg-white p-0 rounded-lg">
-        <DialogTitle className="sr-only">One Health Member Plan</DialogTitle>
+        <DialogHeader className="sr-only">
+          <DialogTitle>One Health Member Plan</DialogTitle>
+        </DialogHeader>
         
         <div className="grid md:grid-cols-2 items-center">
           <div className="order-2 md:order-1 p-6">
@@ -142,17 +144,14 @@ const SubscriptionButton = () => {
           </ShimmerText>
         
             <DialogFooter className="sm:justify-center mt-4">
-              <Link href="/savings" passHref>
-                <Button asChild className="w-full bg-preventify-green hover:bg-preventify-dark-green text-white text-base py-3 px-6" onClick={() => setIsOpen(false)}>
-                  <a>Claim My Peace of Mind</a>
+              <Link href="/savings" asChild>
+                <Button className="w-full bg-preventify-green hover:bg-preventify-dark-green text-white text-base py-3 px-6" onClick={() => setIsOpen(false)}>
+                  Claim My Peace of Mind
                 </Button>
               </Link>
             </DialogFooter>
         </div>
-        <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </DialogClose>
+        
       </DialogContent>
     </Dialog>
   );
