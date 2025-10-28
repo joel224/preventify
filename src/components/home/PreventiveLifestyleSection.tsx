ddfed8ce57fa5e35dfc332113c01e05f8d7a504c
Jay <joeljoshy224@gmail.com>
2025-10-28 13:50:08 +0000

    Make the following changes:
    Find this element:
    
      <div class="inline-fl

--- src/components/home/PreventiveLifestyleSection.tsx
+++ src/components/home/PreventiveLifestyleSection.tsx
@@ -8,8 +8,8 @@ const PreventiveLifestyleSection = () => {
     return (
         <section className="bg-white py-16 md:py-24 relative">
              <div className="absolute -top-8 left-4 sm:left-6 lg:left-8">
-                    <div className="inline-flex items-center gap-2 bg-white rounded-full p-3 shadow-lg border border-gray-200/80">
-                        <Image src="/logo.png" alt="Preventify Logo" width={40} height={40} />
+                    <div className="inline-flex items-center gap-2 bg-white rounded-full p-4 shadow-lg border border-gray-200/80">
+                        <Image src="/logo.png" alt="Preventify Logo" width={48} height={48} />
                     </div>
                 </div>
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
