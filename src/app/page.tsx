a1c3724f0df31a6d735ad1fc421ac6de7c99c7f4
Jay <joeljoshy224@gmail.com>
2025-10-28 07:59:53 +0000

    remove this  (_for element <DoctorSearch>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import FeaturesSection from "@/components/home/FeaturesSection";
 import BlogPreviewSection from "@/components/home/BlogPreviewSection";
 import LocationsSection from "@/components/home/LocationsSection";
 import CtaSection from "@/components/home/CtaSection";
-import DoctorSearch from "@/components/home/DoctorSearch";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <DoctorSearch />
       <ServicesSection />
       <FeaturesSection />
       <BlogPreviewSection />
