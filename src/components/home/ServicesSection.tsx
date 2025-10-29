13276a8397d22cfb259232cd8f6f5bb8b0a27bb3
Jay <joeljoshy224@gmail.com>
2025-10-29 06:30:15 +0000

    use this   https://cdn-icons-png.flaticon.com/512/9354/9354551.png (_for

--- src/components/home/ServicesSection.tsx
+++ src/components/home/ServicesSection.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 
 const services = [
   {
-    icon: "https://cdn-icons-png.flaticon.com/512/2966/2966334.png",
+    icon: "https://cdn-icons-png.flaticon.com/512/9354/9354551.png",
     title: "Primary Care",
     description: "Comprehensive healthcare services for individuals and families.",
   },
