85c846030e50ce0ff35f4c5fb67f25e4a939a6e6
Jay <joeljoshy224@gmail.com>
2025-11-02 08:54:37 +0000

    its not loading

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -280,6 +280,7 @@ const ContactPage = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.348160943641!2d76.0180613!3d10.7846239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7b90b671ae7ad%3A0x578848312f43e2e0!2sPreventify%20Hospital%20Clinic!5e0!3m2!1sen!2sin!4v1747716474838!5m2!1sen!2sin"
                   width="600"
                   height="450"
+                  style={{border:0}}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
@@ -290,6 +291,7 @@ const ContactPage = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.348160943641!2d76.0180613!3d10.7846239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7b90b671ae7ad%3A0x578848312f43e2e0!2sPreventify%20Hospital%20Clinic!5e0!3m2!1sen!2sin!4v1747716474838!5m2!1sen!2sin"
                   width="600"
                   height="450"
+                  style={{border:0}}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
@@ -352,4 +354,5 @@ const ContactPage = () => {
 
 export default ContactPage;
 
+    
     
