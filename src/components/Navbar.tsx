3b3fc76a4c3b6d9f16291ffb582e6cc90a7b34c3
Jay <joeljoshy224@gmail.com>
2025-12-05 16:08:21 +0000

    can you push the things from here to a bit right side ? everything is sp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -113,7 +113,7 @@ const Navbar = () => {
                 {link.name}
               </Link>
             ))}
-            <a href="tel:+918129334858" className="group flex items-center gap-1 hover:text-primary transition-all text-xl text-black transform-origin-left group-hover:scale-110 hover:text-2xl">
+            <a href="tel:+918129334858" className="group flex items-center gap-1 hover:text-primary transition-all text-xl text-black transform-origin-left group-hover:scale-110">
               <Phone className="h-4 w-3 " />
               +91 8129334858
             </a>
@@ -139,7 +139,7 @@ const Navbar = () => {
             </Link>
           </div>
 
-          <nav className="hidden lg:flex space-x-2 items-center">
+          <nav className="hidden lg:flex items-center">
             <NavigationMenu>
               <NavigationMenuList>
                 <NavigationMenuItem>
